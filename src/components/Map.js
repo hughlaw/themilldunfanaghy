a98efe3f69fa6a4f67873d51572ad07215cb9afc
Hugh Law <hugh.law@terminalfour.com>
2020-05-22 09:00:13 +0100

    update maps API and add components

---
+++ src/components/Map.js
@@ -0,0 +1,204 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import mapIcon from '../img/map-marker.png';
+
+const Marker = () => {
+  const style = {
+    position: 'absolute',
+    transform: 'translate(-50%, -100%)',
+    filter: 'drop-shadow(0 0px 1px rgba(0,0,0,.3))',
+  };
+  return <img src={mapIcon} alt="" width="64" height="92" style={style} />;
+};
+
+const createMapOptions = () => {
+  return {
+    styles: [
+      {
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#f5f5f5',
+          },
+        ],
+      },
+      {
+        elementType: 'labels.icon',
+        stylers: [
+          {
+            visibility: 'off',
+          },
+        ],
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#616161',
+          },
+        ],
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [
+          {
+            color: '#f5f5f5',
+          },
+        ],
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#bdbdbd',
+          },
+        ],
+      },
+      {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#eeeeee',
+          },
+        ],
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#757575',
+          },
+        ],
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#e5e5e5',
+          },
+        ],
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#9e9e9e',
+          },
+        ],
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#ffffff',
+          },
+        ],
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#757575',
+          },
+        ],
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#dadada',
+          },
+        ],
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#616161',
+          },
+        ],
+      },
+      {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#9e9e9e',
+          },
+        ],
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#e5e5e5',
+          },
+        ],
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#eeeeee',
+          },
+        ],
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#c9c9c9',
+          },
+        ],
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#9e9e9e',
+          },
+        ],
+      },
+    ],
+  };
+};
+
+export default function Map() {
+  const markerLocation = {
+    lat: 55.176901,
+    lng: -7.979223,
+  };
+
+  return (
+    <section id="find-us" className="no-padding no-margin">
+      <h2>Find us</h2>
+      <div style={{ height: '40vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyB0NyLEx5VVudy1pV4VuECcr6TqvMmcZQU' }}
+          defaultZoom={10}
+          defaultCenter={markerLocation}
+          // options={createMapOptions}
+        >
+          <Marker
+            lat={markerLocation.lat}
+            lng={markerLocation.lng}
+            text="The Mill Dunfanaghy"
+          />
+        </GoogleMapReact>
+      </div>
+    </section>
+  );
+}
